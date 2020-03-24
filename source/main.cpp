94e3efc956ce7c732ade9cbebdd0e69cff57485b
Skylighty <48515992+Skylighty@users.noreply.github.com>
2020-03-24 13:23:19 +0100

    Add project files.

---
+++ source/main.cpp
@@ -0,0 +1,59 @@
+#include <Windows.h>
+#include <cstdio>
+#include "res.h"
+using namespace std;
+
+HINSTANCE g_hInstance;
+
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  switch(uMsg)
+  {
+  case WM_INITDIALOG:
+    {
+    HICON hIcon = LoadIcon(g_hInstance, MAKEINTRESOURCE(IDI_FIRST));
+    SendMessage(hwndDlg, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+    return 0;
+    }
+ /* case WM_COMMAND:
+    {
+      switch(HIWORD(wParam))
+      {
+      case BN_CLICKED:
+        switch(LOWORD(wParam))
+        {
+        case IDC_BUTTON1:
+          MessageBox(hwndDlg, "DZIALA", "WOW", MB_ICONINFORMATION | MB_OK);
+          break;
+        }
+      }
+    }
+    return 0;
+   */
+  case WM_CLOSE:
+    {
+    DestroyWindow(hwndDlg);
+    PostQuitMessage(0);
+    break;
+    }
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  }
+  return 0;
+}
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR nCmdLine, int iCmdShow)
+{
+  g_hInstance = hInstance;
+  HWND hwndMainWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MAINVIEW), NULL, DialogProc);
+  ShowWindow(hwndMainWindow, iCmdShow);
+
+  MSG msg = {};
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return 0;
+}
---
+++ source/res.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by res.rc
+//
+#define IDD_MAINVIEW                    101
+#define IDI_FIRST                       107
+#define IDI_SECOND                      108
+#define IDR_MENU1                       109
+#define IDC_BUTTON1                     1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

be0fd265330bfbd25a184472253d11d14ec0efee
Skylighty <48515992+Skylighty@users.noreply.github.com>
2020-03-24 14:02:10 +0100

    Two working windows, different icons, identification buttons.

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 HINSTANCE g_hInstance;
+HINSTANCE j_hInstance;
 
 INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -15,7 +16,7 @@ INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPara
     SendMessage(hwndDlg, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
     return 0;
     }
- /* case WM_COMMAND:
+  case WM_COMMAND:
     {
       switch(HIWORD(wParam))
       {
@@ -23,13 +24,12 @@ INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPara
         switch(LOWORD(wParam))
         {
         case IDC_BUTTON1:
-          MessageBox(hwndDlg, "DZIALA", "WOW", MB_ICONINFORMATION | MB_OK);
+          MessageBox(hwndDlg, "Ja jestem KAJKO!", "Hejka!", MB_ICONINFORMATION | MB_OK);
           break;
         }
       }
     }
     return 0;
-   */
   case WM_CLOSE:
     {
     DestroyWindow(hwndDlg);
@@ -42,12 +42,53 @@ INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPara
   }
   return 0;
 }
+INT_PTR CALLBACK SecProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  switch(uMsg)
+  {
+  case WM_INITDIALOG:
+    {
+    HICON hIcon = LoadIcon(j_hInstance, MAKEINTRESOURCE(IDI_SECOND));
+    SendMessage(hwndDlg, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+    return 0;
+    }
+  case WM_COMMAND:
+  {
+    switch (HIWORD(wParam))
+    {
+    case BN_CLICKED:
+      switch (LOWORD(wParam))
+      {
+      case IDC_BUTTON1:
+        MessageBox(hwndDlg, "Ja jestem KOKOSZ!", "Dziendoberek!", MB_ICONINFORMATION | MB_OK);
+        break;
+      }
+    }
+  }
+  return 0;
+  case WM_CLOSE:
+    {
+      DestroyWindow(hwndDlg);
+      PostQuitMessage(0);
+      break;
+    }
+  case WM_DESTROY:
+    {
+      PostQuitMessage(0);
+      break;
+    }
+  }
+  return 0;
+}
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR nCmdLine, int iCmdShow)
 {
   g_hInstance = hInstance;
+  j_hInstance = hInstance;
   HWND hwndMainWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MAINVIEW), NULL, DialogProc);
+  HWND hwndSecondWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_SECVIEW), NULL, SecProc);
   ShowWindow(hwndMainWindow, iCmdShow);
+  ShowWindow(hwndSecondWindow, iCmdShow);
 
   MSG msg = {};
   while (GetMessage(&msg, NULL, 0, 0))
--- source/res.h
+++ source/res.h
@@ -3,6 +3,7 @@
 // Used by res.rc
 //
 #define IDD_MAINVIEW                    101
+#define IDD_SECVIEW                     102
 #define IDI_FIRST                       107
 #define IDI_SECOND                      108
 #define IDR_MENU1                       109
@@ -12,7 +13,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
